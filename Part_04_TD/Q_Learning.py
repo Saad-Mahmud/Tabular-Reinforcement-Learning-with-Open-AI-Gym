3a74d7131d410d4ccfe8f734ce3d9cf71031478c
Saad-Mahmud <saadmahmud14@gmail.com>
2020-04-07 01:40:07 +0600

    Update Q_Learning.py

--- Part_04_TD/Q_Learning.py
+++ Part_04_TD/Q_Learning.py
@@ -3,7 +3,7 @@ import gym
 from collections import defaultdict
 from tensorboardX import SummaryWriter
 
-class onMC_Agent(object):
+class QL_Agent(object):
 
     def __init__(self, env, maxEpi=100000, gamma = .95, epsilon = 0.1, alpha = 0.01):
         self.env = env
@@ -70,5 +70,5 @@ class onMC_Agent(object):
 
 if __name__ == "__main__":
     env = gym.make("FrozenLake-v0").env
-    agent = onMC_Agent(env)
+    agent = QL_Agent(env)
     agent.learn()
